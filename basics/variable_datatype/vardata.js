b851705b08f875b430bffbd7f5d77d09cf5f4a8d
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2023-10-24 09:02:19 +0000

    js

---
+++ basics/variable_datatype/vardata.js
@@ -0,0 +1,16 @@
+const name="SIDDHANT";// given name now cannot be changes as it is declared as constant
+
+ let id="siddhanttomar2003";
+
+ var pwd="abc@7829";//var is not used to due to functional scope
+ 
+ city="GHAZIABAD"//"NOT PREFFERED"   WE CAN ALSO DECLARE ANYTHING WITHOUT USING LET, VAR ,CONST KEYWORD
+ console.table([name,id,pwd,city])//used to print in tabular form
+
+// mostly data type are as same of the c++
+//null is the datatype here
+//symbol is the datatype
+console.log(typeof null)
+
+
+ 
