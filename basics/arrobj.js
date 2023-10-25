3d3108823475a604580a0e524482d7341cc63c05
siddhanttomar2003 <141900451+siddhanttomar2003@users.noreply.github.com>
2023-10-25 09:28:22 +0000

    arrobj

---
+++ basics/arrobj.js
@@ -0,0 +1,14 @@
+// array declaration
+let myarr=[1,2,3];// just use this []to declare this
+console.log(myarr[0]);
+//object declaration
+let myobj ={
+    nam: "sid",
+    id: 4,
+}
+//function declaration
+const firfunc= function ll(){
+    console.log("hello world");
+}
+
+
